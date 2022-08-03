4de1230141e4b4c8ef812b08e922c1bb7432dce6
Antonionavarrete4 <antonio.navarrete@talenttools.net>
2022-08-03 12:28:23 +0200

    v1

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '77mui2frlq54nl', // your App ID
+      'clientSecret': 'mfiXMm71QrDyXNgr', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
